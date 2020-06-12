9f118518c7223f6b44ec361395e4df0dda1a5e71
Carter Smith <carters3@illinois.edu>
2020-06-12 11:32:43 -0400

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Carter Smith on 6/12/20.
+//  Copyright © 2020 Carter Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
